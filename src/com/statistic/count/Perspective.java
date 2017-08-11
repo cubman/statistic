1197a34898fe1fc81efbe83edc2795e55bba8c39
cubman <tolik.abramoff@yandex.ru>
2017-08-11 13:46:58 +0400

    add views

--- src/com/statistic/count/Perspective.java
+++ src/com/statistic/count/Perspective.java
@@ -3,9 +3,15 @@ package com.statistic.count;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import Views.DiscroptionView;
+import Views.ExplorerView;
+
 public class Perspective implements IPerspectiveFactory {
 
 	@Override	
 	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.addView(ExplorerView.ID, IPageLayout.LEFT, 0.4f, layout.getEditorArea());
+		layout.addView(DiscroptionView.ID, IPageLayout.RIGHT, 0.5f, layout.getEditorArea());
 	}
 }
