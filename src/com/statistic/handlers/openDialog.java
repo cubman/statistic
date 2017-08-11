320c5ca2a791d1a583b17c2c0dad2333083a41e8
cubman <tolik.abramoff@yandex.ru>
2017-08-11 14:08:43 +0400

    handler added

---
+++ src/com/statistic/handlers/openDialog.java
@@ -0,0 +1,18 @@
+package com.statistic.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+public class openDialog extends AbstractHandler implements IHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent a_event) throws ExecutionException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
